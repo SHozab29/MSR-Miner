29d25da4765c21052ebdf197f7836490a52fc89e
SHozab <53463245+SHozab29@users.noreply.github.com>
2023-09-20 22:19:28 +0530

    added delay to reduce RAM loads

--- MSR miner.py
+++ MSR miner.py
@@ -1,9 +1,37 @@
 import webbrowser
 import random
+import time
+
+
+#Final Draft after adding timer to reduce RAM load
+
+
+with open('words.txt','r') as file:
+    allWords = file.read()
+    words = list(map(str,allWords.split()))
+
+def miner():
+    word = random.choice(words)
+    print(word + '\n')
+    webbrowser.open(f'https://www.bing.com/search?q={word}')
 
 
 
-#Final Draft
+seconds_to_delay_search = 1 # <----------------- SET TO 0 IF SYSTEM RAM > 8GB
+
+for i in range(0,34):
+    time.sleep(seconds_to_delay_search)
+    print('Search no: ',i+1)
+    miner()
+
+
+'''
+Timer working accurately1   
+dated: 20/09/2023 22:24     
+'''
+
+'''   
+#Final Draft of 11/06/2023 00:20
 
 with open('words.txt','r') as file:
     allWords = file.read()
@@ -15,13 +43,14 @@ def miner():
     webbrowser.open(f'https://www.bing.com/search?q={word}')
 
 for i in range(0,34):
+    print('Search no: ',i+1)
     miner()
 
-
+'''
 
 '''
 Accurately doing random and non repeating searches on bing
-dated: 11/06/2024 00:20
+dated: 11/06/2023 00:20
 '''
 
 
